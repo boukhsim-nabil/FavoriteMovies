40bd8b8e5c207122d3a7f0c39cef39d1d6ff4ecf
boukhsim nabil <nabilboukhsim@gmail.com>
2025-02-07 15:39:11 +0100

    Correction du composant Home et mise à jour des films

--- movie/src/App.jsx
+++ movie/src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import MovieCard from '../component/MovieCard';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const movieNumber =1;
 
   return (
-<div><div/>
-  )
+    <>
+    
+      <MovieCard movie={{title:"lord of the rings",release_date:"2005"}}/>
+      <MovieCard movie={{title:"lord of the rings 2",release_date:"2005"}}/>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ movie/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home(){
+    const movies = [
+        {id: 1, title: "John wihk", release_date: "2020"},
+        {id: 2, title: "John wihk2", release_date: "2010"},
+        {id: 3, title: "John wihk3", release_date: "2020"},
+        {id: 4, title: "John wihk4", release_date: "2020"},
+    ]
+    return
+
+}
